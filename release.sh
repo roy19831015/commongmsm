0b833f671cdfd5a9352cfab71aa6e27ae0737867
roy19831015@gmail.com <roy19831015@gmail.com>
2024-02-28 10:08:33 +0800

    第v1.0.20240228100833版本

--- release.sh
+++ release.sh
@@ -3,13 +3,13 @@ echo "git commit -a -m '第v1.0.$DATE版本'"
 git commit -a -m "第v1.0.$DATE版本"
 echo "git checkout -b release/v1.0.$DATE"
 git checkout -b release/v1.0.$DATE
-echo "git push -u origin release/v1.0.$DATE"
-git push -u origin release/v1.0.$DATE
+echo "git push -u fork release/v1.0.$DATE"
+git push -u fork release/v1.0.$DATE
 echo "git tag v1.0.$DATE"
 git tag v1.0.$DATE
 echo "git push --tags"
 git push --tags
-git checkout master
-echo "git checkout master"
+git checkout main
+echo "git checkout main"
 echo "goreleaser release --snapshot --rm-dist"
 goreleaser release --snapshot --rm-dist
