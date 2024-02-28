fe30c6ba9b55a474d0d32ba99ccf457d8ec22bcd
roy19831015@gmail.com <roy19831015@gmail.com>
2024-02-28 10:03:46 +0800

    第v1.0.20240228100346版本

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@ echo "git commit -a -m '第v1.0.$DATE版本'"
 git commit -a -m "第v1.0.$DATE版本"
 echo "git checkout -b release/v1.0.$DATE"
 git checkout -b release/v1.0.$DATE
-echo "git push -u origin release/v1.0.$DATE"
-git push -u origin release/v1.0.$DATE
+echo "git push -u fork release/v1.0.$DATE"
+git push -u fork release/v1.0.$DATE
 echo "git tag v1.0.$DATE"
 git tag v1.0.$DATE
 echo "git push --tags"
